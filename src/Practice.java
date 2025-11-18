c6600a7190553ecfc3950a1d6255b52a258715a1
ryanrmills <ryanroymills@gmail.com>
2025-11-18 14:51:33 -0800

    experimented with hexadecimals

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,25 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Yay bitwise operations!");
+        int color = 0xc4246D;
+        int blueMask = 0x0000FF;
+
+        int blue = color & blueMask;
+        int greenMask = 0x00FF00;
+        int green = (color & greenMask) >> 8;
+
+        // System.out.println(Integer.toHexString(blue));
+        System.out.println(Integer.toHexString(green));
+
+        // System.out.println("Yay bitwise operations!");
+        // int x = 0b10101;
+        // int y = 0xcf4e;
+        // int z = 94;
+        // int p = 0b11001;
+        // int q = x & p;
+
+        // System.out.println(Integer.toBinaryString(z));
+        // System.out.println(Integer.toBinaryString(q));
+        // System.out.println(Integer.toHexString(z));
+
     }
 }
